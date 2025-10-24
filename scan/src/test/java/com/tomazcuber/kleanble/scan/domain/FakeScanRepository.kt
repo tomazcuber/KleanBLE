38271fca9bb99efcfc25ae6a00dceb017c8e063d
tomazcuber <tomazcuber@outlook.com>
2025-10-24 14:54:38 -0300

    tests: add fakescanrepository for unit tests

--- scan/src/test/java/com/tomazcuber/kleanble/scan/domain/FakeScanRepository.kt
+++ scan/src/test/java/com/tomazcuber/kleanble/scan/domain/FakeScanRepository.kt
@@ -4,25 +4,22 @@ import com.tomazcuber.kleanble.scan.domain.model.BleScanFilter
 import com.tomazcuber.kleanble.scan.domain.model.BleScanResult
 import com.tomazcuber.kleanble.scan.domain.model.BleScanSettings
 import com.tomazcuber.kleanble.scan.domain.model.BleScanState
-import com.tomazcuber.kleanble.scan.domain.repository.LiveScanDataSource
+import com.tomazcuber.kleanble.scan.domain.repository.ScanRepository
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 /**
- * A fake implementation of the [LiveScanDataSource] for use in unit tests.
- *
- * This class allows for controlling the state and results flows and for verifying
- * that data source methods are called as expected.
+ * A fake implementation of the [ScanRepository] for use in unit tests.
  */
-internal class FakeScanDataSource : LiveScanDataSource {
+internal class FakeScanRepository : ScanRepository {
 
     private val _scanState = MutableStateFlow<BleScanState>(BleScanState.Idle)
     override val scanState = _scanState.asStateFlow()
 
-    private val _scanResults = MutableSharedFlow<BleScanResult>()
-    override val scanResults = _scanResults.asSharedFlow()
+    private val _scanResults = MutableStateFlow<List<BleScanResult>>(emptyList())
+    override val scanResults = _scanResults.asStateFlow()
 
     var startScanCalled = false
         private set
@@ -50,7 +47,7 @@ internal class FakeScanDataSource : LiveScanDataSource {
         _scanState.value = state
     }
 
-    suspend fun emitScanResult(result: BleScanResult) {
-        _scanResults.emit(result)
+    fun emitScanResultList(results: List<BleScanResult>) {
+        _scanResults.value = results
     }
 }
